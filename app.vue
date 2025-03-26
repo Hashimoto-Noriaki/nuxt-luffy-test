f980971fef3163b65a4db820e0df95c07b74e007
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-26 23:41:15 +0900

    親でPropsを渡す準備

--- app.vue
+++ app.vue
@@ -1,8 +1,41 @@
+<script setup lang="ts">
+//キャラクタリストデータ
+const characterListInit = new Map<number,Character>();
+characterListInit.set(1, {id:1, name:"ルフィ", bounty: 100000000})
+characterListInit.set(2, {id:2, name:"ゾロ", bounty: 60000000})
+const characterList = ref(characterListInit);
+
+//懸賞金の合計の算出プロパティ
+const totalBounty = computed(
+  (): number => {
+    let total = 0;
+    for(const character of characterList.value.values()){
+      total += character.bounty;
+    }
+    return total;
+  }
+);
+
+//キャラクターインターフェイス
+interface Character {
+  id:number;
+  name:string;
+  bounty:number;
+}
+
+
+</script>
+
+
 <template>
   <section>
-    <h1>キャラクター名</h1>
-    <Nami/><!-- Nuxt.jsはオールインポート-->
-    <MonkeyLuffy/><!-- サブフォルダも自動インポートでサブフォルダ名も含める-->
+    <h1>キャラクターリスト</h1>
+    <p>懸賞金の合計: {{ totalBounty }}</p>
+    <OneCharacter
+      v-for="[id,character] in characterList"
+      v-bind:key="id"
+      v-bind:name="character.name"
+      v-bind:bounty="character.bounty"/>
   </section>
 </template>
 
